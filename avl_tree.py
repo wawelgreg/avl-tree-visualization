05746598144e05914d0873433f640b04fd7e9464
wawelgreg <gregloj2001@gmail.com>
2024-03-16 16:47:36 +0100

    Create avl_tree.py

---
+++ avl_tree.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import node
+
+class AVLTree:
+	def __init__(self):
+		self.root = None
+	
+		
+if __name__ == "__main__":
+	tree = AVLTree()
+	tree.root = node.Node(5)
+	print "Root payload", tree.root.payload
