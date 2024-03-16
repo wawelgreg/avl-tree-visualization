573c35cc15fe205a300747a5bf7341d9a7b10e9f
wawelgreg <gregloj2001@gmail.com>
2024-03-16 16:59:08 +0100

    Test root/left/right relationships

--- avl_tree.py
+++ avl_tree.py
@@ -9,5 +9,15 @@ class AVLTree:
 		
 if __name__ == "__main__":
 	tree = AVLTree()
-	tree.root = node.Node(5)
-	print "Root payload", tree.root.payload
+
+	na = node.Node(2)
+	nb = node.Node(1)
+	nc = node.Node(3)
+
+	tree.root = na
+	tree.root.left = nb
+	tree.root.right = nc
+
+	print "Tree root", tree.root.payload
+	print "Root left child", tree.root.left.payload		
+	print "Root right child", tree.root.right.payload		
