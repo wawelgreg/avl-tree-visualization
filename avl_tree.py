1e29824611055e408794dcf5e5e139c83ad4ae28
wawelgreg <gregloj2001@gmail.com>
2024-03-17 18:51:06 +0100

    Import and config logging

--- avl_tree.py
+++ avl_tree.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
-
 import node
+import logging as log
+
+log.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',level=log.DEBUG, filename='bt.log', datefmt='%Y-%m-%dT%H:%M:%S%z')
+
 
 class AVLTree:
 	def __init__(self):
 		self.root = None
+
 	
 		
 if __name__ == "__main__":
+	log.debug("Test log 1")
 	tree = AVLTree()
 
 	na = node.Node(2)
@@ -21,3 +26,4 @@ if __name__ == "__main__":
 	print "Tree root", tree.root.payload
 	print "Root left child", tree.root.left.payload		
 	print "Root right child", tree.root.right.payload		
+	log.debug("Test log 2")
